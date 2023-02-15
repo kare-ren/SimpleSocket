6026af6fcd6af822c6fd78d8ef6405df436a4e10
nmize1 <50761210+nmize1@users.noreply.github.com>
2023-02-15 14:03:58 -0500

    added stats command

--- server.py
+++ server.py
@@ -1,32 +1,47 @@
 from socket import *
 import threading
 
+clients = {}
+total = 0
+
 def broadcast(message, sender=None):
+    if(sender != None):
+        client = clients[sender]
+        client[1] += 1
     for client in clients:
         client.sendall(message)
 
-def listUsers(message, connectionSocket):
+def listUsers(connectionSocket):
     for value in clients.values():
-        value = value + "\n"
-        connectionSocket.send(value.encode())
+        connectionSocket.send(value[0].encode())
+
+def getStats(connectionSocket):
+    client = clients[connectionSocket]
+    connectionSocket.send((client[0] + " has sent " + str(client[1]) + " messages out of a total of " + str(total) + " messages.").encode())
 
 def clientConnections(connectionSocket, addr):
+    global total
+
     connectionSocket.send("Enter your username:".encode())
     username = connectionSocket.recv(1024).decode()
-    clients[connectionSocket] = username
+    clients[connectionSocket] = [username, 0]
     print("Received connection from " + str(connectionSocket) + " with username " + username)
     connectionSocket.send(("Welcome to the chat " + username).encode())
     try:
          while True:
-             message = connectionSocket.recv(1024).decode()
-             if message.lower() == "/users":
-                listUsers(message, connectionSocket)   
-             else:    
+            message = connectionSocket.recv(1024).decode()
+            if message.lower() == "/users":
+                listUsers(connectionSocket)
+            elif message.lower() == "/stats":
+                getStats(connectionSocket)
+            else:
+                total += 1
+                print(total)
                 message = (username + ": " + message).encode()
                 print(message.decode())
                 broadcast(message, connectionSocket)
 
-    except:
+    except ConnectionResetError:
         connectionSocket.close()
         del clients[connectionSocket]
         broadcast((username + " has left the chat.").encode())
@@ -37,7 +52,6 @@ serverSocket.bind(('', serverPort))
 serverSocket.listen(5)
 print("The server is ready to receive")
 
-clients = {}
 
 while True:
     connectionSocket, addr = serverSocket.accept()
