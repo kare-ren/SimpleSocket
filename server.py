b542e5fedc1174f2fef24558279eaf6e4172597d
Caleb Hagens <89407055+Chagens@users.noreply.github.com>
2023-02-16 21:08:27 -0500

    Update server.py

--- server.py
+++ server.py
@@ -2,55 +2,64 @@ from socket import *
 import threading
 import sys
 
-clients = {}
-total = 0
+class channel:
+    def __init__(self, name, clients, total):
+        self.name = name
+        self.clients = clients
+        self.total = total
+    #throw all functions into object object per channel
+    def broadcast(self, message, sender=None):
+        if(sender != None):
+            client = self.clients[sender]
+            client[1] += 1
+        for client in self.clients:
+            client.sendall(message)
 
-def broadcast(message, sender=None):
-    if(sender != None):
-        client = clients[sender]
-        client[1] += 1
-    for client in clients:
-        client.sendall(message)
+    def listUsers(self, connectionSocket):
+        for value in self.clients.values():
+            connectionSocket.send(value[0].encode())
 
-def listUsers(connectionSocket):
-    for value in clients.values():
-        connectionSocket.send(value[0].encode())
+    def getStats(self, connectionSocket):
+        client = self.clients[connectionSocket]
+        connectionSocket.send((client[0] + "has sent " + str(client[1]) + " messages out of a total of " + str(self.total) + " messages.").encode())
 
-def getStats(connectionSocket):
-    client = clients[connectionSocket]
-    connectionSocket.send((client[0] + " has sent " + str(client[1]) + " messages out of a total of " + str(total) + " messages.").encode())
+    def clientConnections(self, connectionSocket, addr):
+        self.total
+        hostname= gethostname()   
+        IPAddr= gethostbyname(hostname)   
+        connectionSocket.send("Enter your username if no name type N:".encode())
+        username = connectionSocket.recv(1024).decode()
+        if username == 'n' or username == 'N':
+            username = IPAddr
+        self.clients[connectionSocket] = [username, 0] 
+        print("Received connection from " + str(connectionSocket) + " with username " + username)
+        connectionSocket.send(("Welcome to the chat room" + str(self.name) + ":" + username).encode())
+        try:
+            while True:
+                message = connectionSocket.recv(1024).decode()
+                if message.lower() == "/users":
+                    self.listUsers(connectionSocket)
+                elif message.lower() == "/stats":
+                    self.getStats(connectionSocket)
+                else:
+                    self.total += 1
+                    print(self.total)
+                    message = (username + ": " + message).encode()
+                    print(message.decode())
+                    self.broadcast(message, connectionSocket)
 
-def clientConnections(connectionSocket, addr):
-    global total
-
-    connectionSocket.send("Enter your username:".encode())
-    username = connectionSocket.recv(1024).decode()
-    clients[connectionSocket] = [username, 0]
-    print("Received connection from " + str(connectionSocket) + " with username " + username)
-    connectionSocket.send(("Welcome to the chat " + username).encode())
-    try:
-         while True:
-            message = connectionSocket.recv(1024).decode()
-            if message.lower() == "/users":
-                listUsers(connectionSocket)
-            elif message.lower() == "/stats":
-                getStats(connectionSocket)
-            else:
-                total += 1
-                print(total)
-                message = (username + ": " + message).encode()
-                print(message.decode())
-                broadcast(message, connectionSocket)
-
-    except ConnectionResetError:
-        connectionSocket.close()
-        del clients[connectionSocket]
-        broadcast((username + " has left the chat.").encode())
+        except ConnectionResetError:
+            connectionSocket.close()
+            del self.clients[connectionSocket]
+            self.broadcast((username + " has left the chat.").encode())
 
 if len(sys.argv) != 3:
     print("usage: server.py port channels")
     exit()
-
+channelsList = []
+for i in range (int((sys.argv[2]))):
+    instance = channel(i, {}, 0) 
+    channelsList.append(instance)
 serverPort = int(sys.argv[1])
 serverSocket = socket(AF_INET,SOCK_STREAM)
 serverSocket.bind(('', serverPort))
@@ -60,4 +69,8 @@ print("The server is ready to receive")
 
 while True:
     connectionSocket, addr = serverSocket.accept()
-    threading.Thread(target=clientConnections, args=(connectionSocket, addr)).start()
+    channelNum = int(connectionSocket.recv(1024).decode())
+    spot = channelsList[channelNum] 
+    threading.Thread(target=spot.clientConnections, args=(connectionSocket, addr)).start()
+
+    
